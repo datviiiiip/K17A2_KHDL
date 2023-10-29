c19ff0ae9b8348da5a467dbcfe1caee86818ea7c
datviiiiip <ddquang.dhkl17a2hn@sv.uneti.edu.vn>
2023-10-29 16:39:47 +0700

    Chuong05

--- dddd.py
+++ dddd.py
@@ -9,6 +9,9 @@ ten_hang="Sua hop Vina Milk"
 so_luong=5
 don_gia=25000
 tien_phai_tra=125000
+print('ten_hang ', '=',"Sua hop Vina Milk")
+print('so_luong ', '=',5)
+print('don_gia ', '=',25000)
 print('so tien phai tra ', so_luong, '*', don_gia, '=',tien_phai_tra)
 
 
