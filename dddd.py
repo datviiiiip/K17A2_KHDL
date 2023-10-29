ec8d20b0c76903e1370aa63c3f9e203f173b6ebd
datviiiiip <ddquang.dhkl17a2hn@sv.uneti.edu.vn>
2023-10-29 16:13:31 +0700

    chuong05

---
+++ dddd.py
@@ -0,0 +1,16 @@
+print('**  **  ******  **     **     ** ****')
+print('**  **  **      **     **     **   **')
+print('******  ******  **     **     **   **')
+print('**  **  **      **     **     **   **')
+print('**  **  ******  ****** ****** *******')
+#bai3
+#chuong trinh tinh tien phai tra khi mua hang
+ten_hang="Sua hop Vina Milk"
+so_luong=5
+don_gia=25000
+tien_phai_tra=125000
+print('so tien phai tra ', so_luong, '*', don_gia, '=',tien_phai_tra)
+
+
+      
+      
---
+++ hello.py
@@ -0,0 +1,13 @@
+#Day la chuong trinh python dau tien
+print("Hello lop k17a2 khdl")
+#tinh toan co ban
+x=10
+y=5
+tong=15
+hieu=5
+tich=x*y
+thuong=x/y
+print('tong hai so ', x, '+', y, '=',tong)
+print('hieu hai so ', x, '-', y, '=',hieu)
+print('tich hai so ', x, '*', y, '=',tich)
+print('thuong hai so ', x, '/', y, '=',thuong)
