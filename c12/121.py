91422e15c7e67d72ec3d6cce7aa625a0250c85a9
datviiiiip <ddquang.dhkl17a2hn@sv.uneti.edu.vn>
2024-01-08 22:48:12 +0700

    Baitapvn

---
+++ c12/121.py
@@ -0,0 +1,52 @@
+#8.1
+from ham_chuong8 import chuong8_bai1
+chuong8_bai1()
+#8.2
+from ham_chuong8 import chuong8_bai2
+chuong8_bai2()
+#8.3
+from ham_chuong8 import chuong8_bai3
+chuong8_bai3()
+#8.4
+from ham_chuong8 import chuong8_bai4
+chuong8_bai4()
+#8.5
+from ham_chuong8 import chuong8_bai5
+chuong8_bai5()
+#8.6
+from ham_chuong8 import chuong8_bai6
+chuong8_bai6()
+#8.7
+from ham_chuong8 import chuong8_bai7
+chuong8_bai7()
+#8.9
+from ham_chuong8 import chuong8_bai9
+chuong8_bai9()
+#8.10
+from ham_chuong8 import chuong8_bai10
+chuong8_bai10()
+#8.11
+from ham_chuong8 import chuong8_bai11
+chuong8_bai11()
+#8.12
+from ham_chuong8 import chuong8_bai12
+chuong8_bai12()
+#8.13
+from ham_chuong8 import chuong8_bai13
+chuong8_bai13()
+#8.14
+from ham_chuong8 import chuong8_bai14
+chuong8_bai14()
+#8.15
+from ham_chuong8 import chuong8_bai15
+chuong8_bai15()
+#8.16
+from ham_chuong8 import chuong8_bai16
+chuong8_bai16()
+#8.17
+from ham_chuong8 import chuong8_bai17
+chuong8_bai17()
+#8.18
+from ham_chuong8 import chuong8_bai18
+chuong8_bai18()
+
